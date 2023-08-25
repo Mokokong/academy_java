c3be8b2022e1c9f64bae1d9b6e0221eb7237ed9b
Tiiso Mokokong <mokokongjr@gmail.com>
2023-08-24 20:53:19 -0400

    Thursday selfLed and Inst led exercises, Daily challenge still not done

---
+++ src/com/bptn/course/week1/FindOddPosInArray.java
@@ -0,0 +1,24 @@
+package com.bptn.course.week1;
+
+
+
+public class FindOddPosInArray {
+
+    public static void main(String[] args) {
+        
+    	int [] array = {10, 20, 30, 40, 50};
+    	
+    	for (int i = 0; i < array.length; i++) {
+			
+    		if (i%2 == 1) {
+				
+    			
+    			System.out.println(array[i]);
+			}
+    		
+    		
+			
+		}
+    }
+
+}
