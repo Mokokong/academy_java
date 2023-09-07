20e66e26d2c28d3a9eeb0402bdfaffe5bdb7cc27
Tiiso Mokokong <mokokongjr@gmail.com>
2023-09-07 10:59:04 -0400

    Completed coding38: Executing a string

---
+++ src/com/bptn/course/week3/day4/coding38/Main.java
@@ -0,0 +1,15 @@
+package com.bptn.course.week3.day4.coding38;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Create a variable from the MyThread class here.
+    	MyThread thread = new MyThread(50);
+    	
+        // Start the thread here
+    	thread.start();
+    }
+
+}
