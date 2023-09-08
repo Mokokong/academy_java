814b4e085f037afcbd10ecc46e51ea6e547ad533
Tiiso Mokokong <mokokongjr@gmail.com>
2023-09-07 22:12:22 -0400

    Completed coding 39,310,311:Concurrent threads,Implementing runnable & Synchronized block

---
+++ src/com/bptn/course/week3/day4/coding39/MyThread.java
@@ -0,0 +1,27 @@
+package com.bptn.course.week3.day4.coding39;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MyThread extends Thread {
+
+    // Declare an instance variable of type List<Integer>
+	List<Integer> list = new ArrayList <>();
+
+    // Create a parameterized constructor.
+	public MyThread (String name, List<Integer> list) {
+		
+		super(name);
+		this.list=list;
+		
+	}
+
+    // Override the run() method here. 
+	
+	@Override
+	public void run() {
+		
+		ListUtils.insertNextId(list);
+	}
+}
